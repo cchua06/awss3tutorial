edf729f77896c7f637b83d4b870d72915a726bac
Cedric Chua <cchua06@sas.upenn.edu>
2024-12-17 13:00:11 -0800

    Updated file uploading

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField() #add upload_to as a parameter if you want to upload files locally
     uploaded_at = models.DateTimeField(auto_now_add=True)
